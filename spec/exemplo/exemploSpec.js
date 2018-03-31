939fbf49a555f7815ea04aeb8b3626dd316eb435
fabio guelfi <fabioguelfi@MacBook-Air-de-fabio.local>
2018-03-31 11:42:51 -0300

    iniciando versionamento

---
+++ spec/exemplo/exemploSpec.js
@@ -0,0 +1,9 @@
+describe("suit de test de exemplo com nodejs", () => {
+    it("deve demonstrar o uso do jasmine com o nodejs", () => {
+        expect(true).toEqual(true);
+    })
+
+    it("deve demonstrar o uso do karma em modo de esculta", () => {
+        expect(1).toEqual(1)
+    })
+})
